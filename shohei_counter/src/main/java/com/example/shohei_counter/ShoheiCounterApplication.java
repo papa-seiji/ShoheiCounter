f53f7ec2fa6f2c215a97af71fa96f9cc745f1bbc
Seiji Enomoto <iwillbestandbyme@gmail.com>
2024-11-09 18:06:38 +0900

    Spring Bootプロジェクトの初期設定完了_02

---
+++ shohei_counter/src/main/java/com/example/shohei_counter/ShoheiCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.shohei_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoheiCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoheiCounterApplication.class, args);
+	}
+
+}
---
+++ shohei_counter/src/test/java/com/example/shohei_counter/ShoheiCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shohei_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoheiCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
