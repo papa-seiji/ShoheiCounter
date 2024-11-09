5b42afb9ae6edf9a05b5f38a1423b13265b029dc
Seiji Enomoto <iwillbestandbyme@gmail.com>
2024-11-09 17:14:25 +0900

    Spring Bootプロジェクトのセットアップ

---
+++ shoihei_counter/src/test/java/com/example/shoihei_counter/ShoiheiCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoihei_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoiheiCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
