d6a260777e8d64717d2d1a059cccd9490381a6ed
Seiji Enomoto <iwillbestandbyme@gmail.com>
2024-11-10 23:02:20 +0900

    コントローラークラス構築魔面表示まで確認

---
+++ src/test/java/com/example/shoheicounter/ShoheiCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoheicounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoheiCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
